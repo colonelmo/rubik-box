811f7ca6512dc92a750c6f907ff791f7bb10f5b8
Mohammad Nasirifar <m.nasirifar@live.com>
2016-03-30 19:45:46 +0430

    [WIP] filling in the cube class + tests

--- rubik_box/rubik_box.py
+++ rubik_box/rubik_box.py
@@ -1,5 +1,41 @@
 #!/usr/bin/python
+import copy
 
 def isInstalled():
     return 'yes'
     
+class Face():
+    def __init__(self, side_length):
+        self.size = side_length
+        self.squares = [[0 for __ in range(self.size)] for __ in range(self.size)]
+    
+    def __str__(self):
+        return '\n'.join([' '.join(map(str, x)) for x in self.squares])
+    
+class Cube():
+    face_name = {0: "front", 1: "top", 2:"right", 3:"back", 4:"bottom", 5:"left"}
+    face_number = {name:number for number, name in face_name.iteritems()}
+    face_across = {x:(x + 3)%6 for x in range(6)}
+    _face_down = {'bottom':'back', 'top':'front'} 
+    _face_up = {}
+    _face_left = {}
+    _face_right = {}
+
+    
+    def __init__(self, side_length):
+        self.size = side_length
+        self.sides = [Face(self.size) for __ in range(6)]
+    
+    def side(self, side):
+        return self.sides[self.face_number.get(side, side)]
+        
+def out(*args):
+    for cube in args:
+        print cube.side(0)
+        print ''
+
+def main():
+    c = Cube(1)
+
+if __name__ == '__main__':
+    main()        
--- tests/test_basic.py
+++ tests/test_basic.py
@@ -10,7 +10,31 @@ from rubik_box import rubik_box as rubik
 
 class TestInstallation(unittest.TestCase):
     def test(self):
-        self.assertEqual(rubik.isInstalled(), "yes")
+        self.assertEqual(rubik.isInstalled(), 'yes')
 
+face_numbers = dict(front = 0, top = 1, right = 2, back = 3, bottom = 4, left = 5)
+face_names = {number:name for name, number in face_numbers.iteritems()}
+face_acrosses = dict(front='back', back='front', left='right', right='left', top='bottom', bottom='top')
+
+class TestCube(unittest.TestCase):
+    def test_across_faces_names(self):
+        c = rubik.Cube(1)
+        for face, expected_across in face_acrosses.iteritems():
+            got_across_number = c.face_across[face_numbers[face]]
+            got_name_across = c.face_name[got_across_number]
+            self.assertEqual(expected_across, got_name_across)
+             
+    def test_face_number_to_name(self):
+        c = rubik.Cube(1)
+        for i in range(6):
+            self.assertEqual(face_names[i], c.face_name[i])
+
+    def test_face_name_to_number(self):
+        c = rubik.Cube(1)
+        for i in range(6):
+            expected_name = face_names[i];
+            self.assertEqual(c.face_name[i], expected_name)
+
+    
 if __name__ == '__main__':
     unittest.main()
